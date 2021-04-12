5ef7a1cadeea97d16470b0308e88f155eba2b1b6
Joey Boivin <JoBoi9898@gmail.com>
2021-04-12 17:34:41 -0400

    metadata now shows in gui

--- App/Graphic/app.py
+++ App/Graphic/app.py
@@ -6,13 +6,14 @@ class Application:
 
     def __init__(self, data):
         self.data = data
-        icons, priceWidgets, nameWidgets = self.create_widgets()
-        self.show_widgets(icons, priceWidgets, nameWidgets)
+        icons, priceWidgets, nameWidgets, metaWidgets = self.create_widgets()
+        self.show_widgets(icons, priceWidgets, nameWidgets, metaWidgets)
 
     def create_widgets(self):
         icons = []
         priceWidgets = []
         nameWidgets = []
+        metaWidget = []
         for tag, _data in self.data['items'].items():
             path = f'./Graphic/Images/{_data["img-token"]}.png'
             img = ImageTk.PhotoImage(Image.open(path).resize((100,100)))
@@ -22,25 +23,40 @@ class Application:
             priceWidget = Label(root, text=price)
             priceWidgets.append(priceWidget)
 
+            metadata = _data['metadata']
+            display = ""
+            if metadata:
+                for key, value in metadata.items():
+                    display +=  str(key) + ': ' + str(value) + ' \n'
+
+            display = Label(root, text=display)
+            metaWidget.append(display)
+
             name = _data['product-name']
             nameWidget = Label(root, text=name)
             nameWidgets.append(nameWidget)
-        return icons, priceWidgets, nameWidgets
 
-    def show_widgets(self, icons, priceWidgets, nameWidgets):
+
+        return icons, priceWidgets, nameWidgets, metaWidget
+
+    def show_widgets(self, icons, priceWidgets, nameWidgets, metaWidgets):
 
         count = 0
         for icon in icons:
             panel = Label(root, image=icon)
-            panel.grid(row=count, column=0, padx = '50')
+            panel.grid(row=count, column=0, padx = '50', pady='10')
             count+=1
         count = 0
         for name in nameWidgets:
-            name.grid(row=count, column=1, padx = '50')
+            name.grid(row=count, column=1, padx = '50', pady='10')
             count+=1
         count = 0
         for price in priceWidgets:
-            price.grid(row=count,column=2, padx = '50')
+            price.grid(row=count,column=2, padx = '50', pady='10')
+            count +=1
+        count = 0
+        for metadata in metaWidgets:
+            metadata.grid(row=count,column=3, padx = '50', pady='10')
             count +=1
         
         root.mainloop()
