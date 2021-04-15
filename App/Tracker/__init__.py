1bb735d6481d16eb88b477e2f560e60788f09484
Joey Boivin <JoBoi9898@gmail.com>
2021-04-15 19:01:08 -0400

    NeweggTracker object

--- App/Tracker/__init__.py
+++ App/Tracker/__init__.py
@@ -1,6 +1,1 @@
-from Tracker.newegg_tracker import get_data
-from Tracker.newegg_tracker import add_a_new_item
-from Tracker.newegg_tracker import remove_an_existing_item
-from Tracker.newegg_tracker import update_data
-from Tracker.newegg_tracker import fetch_html
-
+from Tracker.newegg_tracker import NeweggTracker
