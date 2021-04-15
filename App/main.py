a269e3d3ffb16672382aa3e3fb48113a4a9282b2
Joey Boivin <JoBoi9898@gmail.com>
2021-04-15 19:01:28 -0400

    NeweggTracker object

--- App/main.py
+++ App/main.py
@@ -27,27 +27,27 @@ def main():
 
     #Cannot update the data before I check if it's the first use
     #TODO: Maybe modify the update_data method so I can update ASAP and get_data after that stored into a variable (cleaner code)
-    if not Tracker.get_data()['items']:
+    if not Tracker.NeweggTracker().get_data()['items']:
         if not args.add:
             print("You have no items tracked yet. Would you like to track one? Execute python main.py -h for help")
             return
 
-    Tracker.update_data()
-    data = Tracker.get_data()
+    Tracker.NeweggTracker().update_data()
+    data = Tracker.NeweggTracker().get_data()
 
     if gui == "Y":
         app = Graphic.Application(data)
     elif gui == 'N':
         if args.add:
-            Tracker.add_a_new_item(args.add)
+            Tracker.NeweggTracker().add_a_new_item(args.add)
         elif args.delete:
-            Tracker.remove_an_existing_item(args.delete)
+            Tracker.NeweggTracker().remove_an_existing_item(args.delete)
         else:
-            for tag, data in Tracker.get_data()['items'].items():
-                print(data['product-name'])
-                print(list(data['history'].values())[-1]) #Gets the most recent price
-                if data['metadata']:
-                    print(data['metadata'])
+            for tag, _data in data['items'].items():
+                print(_data['product-name'])
+                print(list(_data['history'].values())[-1]) #Gets the most recent price
+                if _data['metadata']:
+                    print(_data['metadata'])
                 print(f"https://www.newegg.ca/{tag}")
                 print("-"*150)
                 print()
